bca2eb55387fc951ca7a56ff441f236f7998219a
edwin rivera <waehake@gmail.com>
2018-01-25 15:23:39 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,33 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = 'Looking for a rig.';
+  }
+  
+  engageWarpDrive() {
+    //engage warp drive is 
+    if (this.position === 'Pilot' && this.currentShip !== 'Looking for a rig.') {
+      //set warpdrive to engage
+      this.currentShip.engageWarpDrive = 'engaged';
+    } else {
+      return 'had no effect';
+    }
+  }
+  
+  setsInvisibility() {
+    if (this.position === "Defender" && this.currentShip !== 'Looking for a rig.') {
+      this.currentShip.cloaked = true;
+    }else{
+      return "had no effect";
+    }
+  }
+  
+  chargePhasers() {
+    //set phasers to charged
+    if (this.position === 'Gunner' && this.currentShip !== 'Looking for a rig') {
+      this.currentShip.phasers = 'charged';
+    } else {
+      return 'had no effect';
+    }
+  }
+}
--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,30 @@
-class Spaceship {}
+class Spaceship {
+    constructor(name, crew, phasers, shields) {
+        this.name = name;
+        this.crew = crew;
+        this.phasers = phasers;
+        this.shields = shields;
+        this.phasersCharge = 'uncharged';
+        this.cloaked = false;
+        this.warpDrive = 'disengaged';
+
+        switch(crew.length <= 0) {
+          case true:
+            this.docked = true;
+            break;
+          case false:
+            this.docked = false;
+            break;
+          default:
+            break;
+        }
+        
+        this.sendCrew();
+    }
+    
+    sendCrew() {
+      this.crew.forEach(crewMember => {
+        crewMember.currentShip = this;
+      });
+    }
+}
